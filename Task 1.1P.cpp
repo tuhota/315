9c861fe96d31a9c0dc6203805cfce6699bd25f34
tuhota <96323290+tuhota@users.noreply.github.com>
2023-08-01 20:25:09 +1000

    Update Task 1.1P.cpp

--- Task 1.1P.cpp
+++ Task 1.1P.cpp
@@ -1,9 +1,11 @@
 #include<Servo.h>
 Servo motor;
 
+//variables for sensor input
 int C;
 int raw;
 
+//angle for servo
 int angle = 0;
 
 int sensorPin = A5;
@@ -11,34 +13,34 @@ int servoPin = 7;
 
 void setup()
 {
-  pinMode(sensorPin, INPUT);
-  pinMode(servoPin, OUTPUT);
-  motor.attach(servoPin);
-  
-  Serial.begin(9600);
+	pinMode(sensorPin, INPUT);
+	pinMode(servoPin, OUTPUT);
+	motor.attach(servoPin);
+	
+	Serial.begin(9600);
 }
 
 void loop()
 {  
-  //raw milliVolt input
-  raw = analogRead(A0);
-  //converts raw input in milliVolts to celsius
-  C = (raw - 500) / 10;
-  
-  //display raw input and celsius conversion
-  Serial.print("Raw: ");
-  Serial.println(raw);
-  Serial.print("Celsius: ");
-  Serial.println(C);
-  
-  //advance servo 5 degrees if temperature is greater than 20C
-  if(C > 20)
-  {
-	angle += 5;
-    Serial.print("Advancing, angle now: ");
-  	Serial.println(angle);
-    motor.write(angle);  
-  }
-  
-  delay(1000); 
+	//raw milliVolt input
+	raw = analogRead(A0);
+	//converts raw input in milliVolts to celsius
+	C = (raw - 500) / 10;
+	
+	//display raw input and celsius conversion
+	Serial.print("Raw: ");
+	Serial.println(raw);
+	Serial.print("Celsius: ");
+	Serial.println(C);
+	
+	//advance servo 5 degrees if temperature is greater than 20C
+	if(C > 20)
+	{
+		angle += 5;
+		Serial.print("Advancing, angle now: ");
+		Serial.println(angle);
+		motor.write(angle);  
+	}
+	
+	delay(1000); 
 }
